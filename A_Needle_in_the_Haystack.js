6beede5725b0d5e95d56f655cf2988e9be83f14e
Aahz <balenko9616@gmail.com>
2025-06-22 23:40:09 +0200

    A Needle in the Haystack https://www.codewars.com/kata/56676e8fabd2d1ff3000000c/train/javascript

---
+++ A_Needle_in_the_Haystack.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/56676e8fabd2d1ff3000000c/train/javascript
+function findNeedle(haystack) {
+    for (let i = 0; i < haystack.length; i++) {
+        if (haystack[i] === "needle") {
+            return `found the needle at position ${i}`;
+        }
+    }
+}
