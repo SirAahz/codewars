74ffadbe5feabf4a1fb069c63e460320318cbfe7
Aahz <balenko9616@gmail.com>
2025-06-22 15:39:17 +0200

    Growth of a Population https://www.codewars.com/kata/563b662a59afc2b5120000c6/train/javascript

---
+++ Growth_of_a_Population.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/563b662a59afc2b5120000c6/train/javascript
+function nbYear(p0, percent, aug, p) {
+    let years = 0;
+    for (; p0 < p; years++) {
+        p0 = Math.floor(p0 + p0 * (percent / 100) + aug);
+    }
+    return years;
+}
