c82806414595b4e2e20c751a597be439bb276d63
Aahz <balenko9616@gmail.com>
2025-06-29 13:52:12 +0200

    Simple multiplication https://www.codewars.com/kata/583710ccaa6717322c000105/train/javascript

---
+++ Simple_multiplication.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/583710ccaa6717322c000105/train/javascript
+function simpleMultiplication(number) {
+    if (number % 2 === 0) {
+        return number * 8;
+    }
+    return number * 9;
+}
